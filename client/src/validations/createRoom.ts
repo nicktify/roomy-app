b7a40b869a372a2023ab25b79c12f3ec822efb27
Nicolas Aguilar <aguilarnicolasivan@gmail.com>
2021-05-20 23:52:02 -0300

    no more password on new room

--- client/src/validations/createRoom.ts
+++ client/src/validations/createRoom.ts
@@ -1,23 +1,10 @@
-export const createRoomValidations = (password: string, repeatedPassword: string, name: string) => {
+export const createRoomValidations = (name: string) => {
   let errors = {
     name: '',
-    password: '',
-    repeatedPassword: '',
   };
 
   if (name.length < 3) {
     errors.name = 'Name should be longer than 2 characters.';
-  }
-
-  if (!password) {
-    errors.password = 'Password is required';
-  }
-  else if (password.length > 20 || password.length < 6 || !/[a-z]/.test(password) || !/[A-Z]/.test(password) || !/[1-9]/.test(password)) {
-    errors.password = 'Password is invalid';
-  }
-
-  if (password !== repeatedPassword) {
-    errors.repeatedPassword = 'Passwords not match';
   }
 
   return errors;
