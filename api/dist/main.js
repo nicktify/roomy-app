092bfa2572c9cf67a8cbceb752cd997f6e8cd268
Nicolas Aguilar <aguilarnicolasivan@gmail.com>
2021-05-24 02:42:49 -0300

    cors

--- api/dist/main.js
+++ api/dist/main.js
@@ -6,8 +6,9 @@ const app_module_1 = require("./app.module");
 const common_1 = require("@nestjs/common");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.enableCors();
     app.useGlobalPipes(new common_1.ValidationPipe());
-    await app.listen(process.env.PORT || 3000, '0.0.0.0');
+    await app.listen(process.env.PORT || 4000, '0.0.0.0');
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- api/src/main.ts
+++ api/src/main.ts
@@ -5,7 +5,8 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+  await app.listen(process.env.PORT || 4000, '0.0.0.0');
 }
 bootstrap();
