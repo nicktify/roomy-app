d6576855eb5067a2bd079d67d05e3280130ce365
Nicolas Aguilar <aguilarnicolasivan@gmail.com>
2021-06-03 21:33:19 -0300

    merge changes

--- client/App.tsx
+++ client/App.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import 'react-native-gesture-handler';
 
 import { NavigationContainer } from '@react-navigation/native';
--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -3,16 +3,12 @@ import {
   Switch,
   Route,
 } from "react-router-dom";
-import Home from './components/Home';
 import Form from './components/Form';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path='/'>
-          <Home />
-        </Route>
         <Route path='/reset-password/:userId/validation/:token'>
           <Form />
         </Route>
